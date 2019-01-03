3d3f1af0aa1a6c9a99c85bae80bf408df4118c75
676339784 <676339784@qq.com>
2019-01-03 15:23:13 -0800

    Add dark mode

---
+++ js/dark.js
@@ -0,0 +1,10 @@
+function dark() {
+	// Has to be in rgb format
+	if (document.body.style.backgroundColor == "rgb(47, 48, 51)") {
+		document.body.style.backgroundColor = "#f9f9f9";
+	}
+	else {
+		document.body.style.backgroundColor = "#2f3033";
+	}
+}
+
